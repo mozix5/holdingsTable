a2a87c983150ede9fba06cca6bc7683be219845c
mozix5 <mosinmd827@gmail.com>
2024-04-21 01:38:09 +0530

    removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Card from "./components/Card";
 import { fetchHoldings, groupHoldingsByAssetClass } from "./helpers/helper";
 
